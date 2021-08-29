a348a22bc36464b051d1681979c7ace19084490b
RonierisonMaciel <ronierison.maciel@gmail.com>
2021-08-29 20:55:40 -0300

    upgrade

---
+++ src/gui/ViewController.java
@@ -0,0 +1,48 @@
+package gui;
+
+import java.net.URL;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import gui.util.Alerts;
+import gui.util.Constraints;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+
+public class ViewController implements Initializable {
+	@FXML
+	private TextField txtNumero1;
+	@FXML
+	private TextField txtNumero2;
+	@FXML
+	private Label labelSoma;
+	@FXML
+	private Button btSoma;
+
+	@FXML
+	public void onBtTestAction() {
+		try {
+			Locale.setDefault(Locale.US);
+			double numero1 = Double.parseDouble(txtNumero1.getText());
+			double numero2 = Double.parseDouble(txtNumero2.getText());
+			double soma = numero1 + numero2;
+			labelSoma.setText(String.format("%.2f", soma));
+		} catch (NumberFormatException e) {
+			Alerts.showAlert("Erro", "Error", e.getMessage(), AlertType.ERROR);
+		}
+
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		Constraints.setTextFieldDouble(txtNumero1);
+		Constraints.setTextFieldDouble(txtNumero2);
+		Constraints.setTextFieldMaxLength(txtNumero1, 6);
+		Constraints.setTextFieldMaxLength(txtNumero2, 6);
+		
+	}
+}
