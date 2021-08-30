40ccd7322d3ac2c1f984a31889330bef1f02c527
RonierisonMaciel <ronierison.maciel@gmail.com>
2021-08-29 23:31:37 -0300

    insert combobox

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,48 +1,47 @@
 package gui;
 
 import java.net.URL;
-import java.util.Locale;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
-import gui.util.Alerts;
-import gui.util.Constraints;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.util.Callback;
+import model.entities.Person;
 
 public class ViewController implements Initializable {
-	@FXML
-	private TextField txtNumero1;
-	@FXML
-	private TextField txtNumero2;
-	@FXML
-	private Label labelSoma;
-	@FXML
-	private Button btSoma;
 
 	@FXML
-	public void onBtTestAction() {
-		try {
-			Locale.setDefault(Locale.US);
-			double numero1 = Double.parseDouble(txtNumero1.getText());
-			double numero2 = Double.parseDouble(txtNumero2.getText());
-			double soma = numero1 + numero2;
-			labelSoma.setText(String.format("%.2f", soma));
-		} catch (NumberFormatException e) {
-			Alerts.showAlert("Erro", "Error", e.getMessage(), AlertType.ERROR);
-		}
+	private ComboBox<Person> comboBoxPerson;
 
-	}
+	private ObservableList<Person> obsList;
 
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
-		Constraints.setTextFieldDouble(txtNumero1);
-		Constraints.setTextFieldDouble(txtNumero2);
-		Constraints.setTextFieldMaxLength(txtNumero1, 12);
-		Constraints.setTextFieldMaxLength(txtNumero2, 12);
+		List<Person> list = new ArrayList<>();
+		list.add(new Person(1, "João", "joao@gmail.com"));
+		list.add(new Person(2, "Maria", "maria@gmail.com"));
+		list.add(new Person(3, "Antônio", "antonio@gmail.com"));
 
+		obsList = FXCollections.observableArrayList(list);
+		comboBoxPerson.setItems(obsList);
+
+		//métodos responsável pela customização dos nomes no combobox
+		Callback<ListView<Person>, ListCell<Person>> factory = lv -> new ListCell<Person>() {
+			@Override
+			protected void updateItem(Person item, boolean empty) {
+				super.updateItem(item, empty);
+				setText(empty ? "" : item.getName());
+			}
+		};
+
+		comboBoxPerson.setCellFactory(factory);
+		comboBoxPerson.setButtonCell(factory.call(null));
 	}
 }
